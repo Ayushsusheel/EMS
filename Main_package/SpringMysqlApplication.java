4bed227a09d7805d0b8d0a0e10ae2aa1a3017e86
Ayush S <60178558+Ayushsusheel@users.noreply.github.com>
2023-08-04 03:12:52 +0530

    main

---
+++ Main_package/SpringMysqlApplication.java
@@ -0,0 +1,12 @@
+package Main_package;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class SpringMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlApplication.class, args);
+	}
+
+}
